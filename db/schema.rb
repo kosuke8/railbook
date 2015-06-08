78794d391fce277ef1679cde05ab5508e4112562
kosuke.takahashi <kosuke8@gmail.com>
2015-06-08 13:50:31 +0900

    使用DBをsqliteからmysqlへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,13 @@
 ActiveRecord::Schema.define(version: 20150605065817) do
 
   create_table "books", force: :cascade do |t|
-    t.string   "isbn"
-    t.string   "title"
-    t.integer  "price"
-    t.string   "publish"
+    t.string   "isbn",       limit: 255
+    t.string   "title",      limit: 255
+    t.integer  "price",      limit: 4
+    t.string   "publish",    limit: 255
     t.date     "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
